11c91f243a374b8b40c2ec7340c5d461619fd894
Kirill Nikitin <132838999+kkkkkknn@users.noreply.github.com>
2024-01-03 20:02:43 +0300

    test commit

--- PlayingField.py
+++ PlayingField.py
@@ -26,3 +26,21 @@ class PlayingField:
                 pygame.draw.rect(screen, pygame.Color(0, 100, 0), (x * self.cell_size + self.left,
                                                                    y * self.cell_size + self.top,
                                                                    self.cell_size, self.cell_size), 1)
+
+
+if __name__ == "__main__":
+    pygame.init()
+    pygame.display.set_caption("Минные поля")
+    size = width, height = 1600, 800
+    screen = pygame.display.set_mode(size)
+
+    board = PlayingField(18, 18)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill((173, 255, 47))
+        board.render(screen)
+        pygame.display.flip()
+    pygame.quit()
